c0e1fee1c902c0aedc3d8137b01426508b5a0a28
Koray Otağ <korayotag@gmail.com>
2023-02-02 02:11:26 +0300

    202302020211

--- app/src/main/java/com/example/m2ivocabo/MainActivity.kt
+++ app/src/main/java/com/example/m2ivocabo/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.m2ivocabo
 
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.BarcodeView
+import com.journeyapps.barcodescanner.CaptureManager
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var permissionDenied = false
     private lateinit var map: GoogleMap
-    private val LOCATION_PERMISSION_REQUEST_CODE=100
+    private var barcodeView: BarcodeView? = null
+
+    lateinit var captureManager: CaptureManager
+    private val LOCATION_PERMISSION_REQUEST_CODE = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        barcodeView = findViewById(R.id.barcodeView) as BarcodeView
+        var captureManager = CaptureManager(this, barcodeView)
+        captureManager.initializeFromIntent(intent, savedInstanceState)
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
     }
@@ -72,4 +86,22 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             LOCATION_PERMISSION_REQUEST_CODE
         )
     }
+
+
+    //start::Scanner events-->
+    private fun ScannerOnClick() {
+
+
+        barcodeView.decodeSingle(object : BarcodeCallback {
+            override fun barcodeResult(result: BarcodeResult?) {
+                result?.let {
+                    Toast.makeText(baseContext, "Result : " + it.text, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+    }
+
+
+    //end::Scanner events-->
 }
