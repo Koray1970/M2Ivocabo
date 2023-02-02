bae57feea4cdf1b72b9d711f318d9ccd58aa3399
Koray Otağ <korayotag@gmail.com>
2023-02-03 01:18:45 +0300

    202302030118

--- app/src/main/java/com/example/m2ivocabo/MainActivity.kt
+++ app/src/main/java/com/example/m2ivocabo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.m2ivocabo
 
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Button
+import android.widget.PopupMenu.OnDismissListener
 import android.widget.Toast
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -14,7 +18,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.journeyapps.barcodescanner.*
+import java.util.zip.Inflater
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -22,19 +28,19 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
     private var scanOptions: ScanOptions? = null
     private var btnaddbeacon: Button? = null
-
+    var addBeaconBuilder: MaterialAlertDialogBuilder? = null
+    var addBeaconDialog: AlertDialog? = null
     private val LOCATION_PERMISSION_REQUEST_CODE = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
+        MaterialAlertDialogBuilder_OnInit()
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
     }
 
+    //start::Barcode Scanner event init
     private val barcodeLauncher = registerForActivityResult(
         ScanContract()
     ) { result: ScanIntentResult ->
@@ -54,25 +60,54 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         scanOptions!!.setBarcodeImageEnabled(true)
         scanOptions!!.setBeepEnabled(true)
 
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            barcodeLauncher.launch(scanOptions)
+        else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                100
+            )
+        }
+    }
+    //end::Barcode Scanner event init
+    //start::MaterialAlertDialog events
+    fun MaterialAlertDialogBuilder_OnInit(){
+        var addDeviceChooseType: View =
+            layoutInflater.inflate(R.layout.fragment_add_device_choose_type, null)
+
+        var btnscancode: Button =
+            addDeviceChooseType.findViewById<Button>(R.id.btnchoosescan)
+        btnscancode.setOnClickListener {
+            SetBarcodeScanning()
+            addBeaconDialog?.dismiss()
+        }
 
         btnaddbeacon = findViewById(R.id.btnaddbeacon) as Button
         btnaddbeacon!!.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.CAMERA
-                ) == PackageManager.PERMISSION_GRANTED
-            )
-                barcodeLauncher.launch(scanOptions)
-            else {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(android.Manifest.permission.CAMERA),
-                    100
-                )
-            }
-        }
-    }
+            addBeaconBuilder =
+                MaterialAlertDialogBuilder(this, R.style.myFullscreenAlertDialogStyle)
+                    .setTitle("")
+                    .setView(addDeviceChooseType)
+                    .setNegativeButton("Cancel", null)
+                    .setOnDismissListener(DialogInterface.OnDismissListener {
+                        (addDeviceChooseType.parent as ViewGroup).removeView(addDeviceChooseType)
 
+                    })
+            addBeaconDialog = addBeaconBuilder!!.create()
+            addBeaconDialog!!.window!!.setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.MATCH_PARENT
+            )
+            addBeaconDialog!!.show()
+        };
+
+    }
+    //end::MaterialAlertDialog events
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         val sydney = LatLng(-33.852, 151.211)
