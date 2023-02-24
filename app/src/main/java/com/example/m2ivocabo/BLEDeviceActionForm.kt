259fec941a8f90bc34c814cf8d05f808d80f00e6
Koray Otağ <korayotag@gmail.com>
2023-02-24 12:07:33 +0300

    202302241207

--- app/src/main/java/com/example/m2ivocabo/BLEDeviceActionForm.kt
+++ app/src/main/java/com/example/m2ivocabo/BLEDeviceActionForm.kt
@@ -1,18 +1,15 @@
 package com.example.m2ivocabo
 
 import android.Manifest
+import android.Manifest.permission.*
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -27,75 +24,52 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.gson.Gson
 
 
 class BLEDeviceActionForm : AppCompatActivity(), OnMapReadyCallback {
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    @RequiresPermission(
-        allOf = arrayOf(
-            android.Manifest.permission.BLUETOOTH_SCAN,
-            android.Manifest.permission.BLUETOOTH_CONNECT
-        )
-    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bledevice_action_form)
         onBackPressedDispatcher.addCallback(onBackPressedCallback)
-
-
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment?.getMapAsync(this)
 
         var bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
         var bluetoothAdapter = bluetoothManager.adapter
         if (!bluetoothAdapter.isEnabled) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-
             ActivityCompat.startActivityForResult(
-                this,
+                this@BLEDeviceActionForm,
                 enableBtIntent,
                 BluetoothTrackService.REQUEST_ENABLE_BT,
                 null
             )
-        } else {
-            //if (Build.VERSION.SDK_INT >= 31)
-                bluetoothscanpermissions.launch(
-                    ANDROID_12_BLE_PERMISSIONS
-                )
         }
 
-       /* mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment?.getMapAsync(this)*/
+
+
         btnTracking = findViewById<Button>(R.id.btntrack)
         btnTracking!!.isClickable = false
+
         btnTracking!!.setOnClickListener {
+            /*if (checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.BLUETOOTH_SCAN))
+                    requestPermissions(arrayOf(android.Manifest.permission.BLUETOOTH_SCAN), 300)
+            }*/
             runNotification()
         }
         getCurrentLocation()
     }
 
-    val bluetoothscanpermissions =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-            when {
-                (permissions.getOrDefault(Manifest.permission.BLUETOOTH_SCAN, false)) -> {
-                    btnTracking!!.isClickable = true
-                    Toast.makeText(applicationContext, "Bluetooth tamam!!!", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-        }
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun runNotification() {
         val intBleTrack = Intent(this@BLEDeviceActionForm, BluetoothTrackService::class.java)
-        startForegroundService(intBleTrack)
+        startService(intBleTrack)
+        /*startForegroundService(intBleTrack)*/
     }
 
     private fun mapOnPrepare() {
-        /*if (map?.cameraPosition!!.zoom >= ZOOM_LEVEL)
+        if (map?.cameraPosition!!.zoom >= ZOOM_LEVEL)
             ZOOM_LEVEL = map?.cameraPosition!!.zoom
         Log.v(TAG, "Zoom Level : ${ZOOM_LEVEL}")
 
@@ -107,7 +81,7 @@ class BLEDeviceActionForm : AppCompatActivity(), OnMapReadyCallback {
                 latlng!!,
                 ZOOM_LEVEL
             )
-        )*/
+        )
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -159,7 +133,7 @@ class BLEDeviceActionForm : AppCompatActivity(), OnMapReadyCallback {
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
         )
-        private val ANDROID_12_BLE_PERMISSIONS = arrayOf(
+        private var ANDROID_12_BLE_PERMISSIONS = arrayOf(
             Manifest.permission.BLUETOOTH_SCAN,
             Manifest.permission.BLUETOOTH_CONNECT,
             Manifest.permission.ACCESS_FINE_LOCATION
--- app/src/main/java/com/example/m2ivocabo/BluetoothTrackService.kt
+++ app/src/main/java/com/example/m2ivocabo/BluetoothTrackService.kt
@@ -9,6 +9,7 @@ import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.IBinder
+import android.widget.Toast
 import androidx.annotation.RequiresPermission
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.requestPermissions
@@ -19,17 +20,10 @@ class BluetoothTrackService : Service() {
     override fun onCreate() {
         var bluetoothManager:BluetoothManager=getSystemService(BluetoothManager::class.java)
         bluetoothAdapter= bluetoothManager.adapter
-        if(applicationContext.checkSelfPermission(android.Manifest.permission.BLUETOOTH_SCAN)==PackageManager.PERMISSION_GRANTED) {
-            if (!bluetoothAdapter.isEnabled) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                var ractivity = application.baseContext as Activity
-                startActivityForResult(ractivity, enableBtIntent, REQUEST_ENABLE_BT, null)
-                stopSelf()
-            }
+        if(checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(applicationContext,"Herşey yolunda",Toast.LENGTH_SHORT).show()
         }
-        else{
-           ActivityCompat.requestPermissions(applicationContext as Activity, arrayOf(android.Manifest.permission.BLUETOOTH_SCAN,android.Manifest.permission.BLUETOOTH_CONNECT),100)
-        }
+
     }
 
 
